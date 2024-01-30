21c44389898218b475d18d304b3a6f5b89b16921
Klubin <clubin2004@gmail.com>
2024-01-30 18:02:00 +0200

    Добавьте файлы проекта.

---
+++ Pr5/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Pr5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button1.Location = new System.Drawing.Point(110, 587);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 89);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Кинуть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button2.Location = new System.Drawing.Point(567, 587);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 89);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Кинуть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.label1.Location = new System.Drawing.Point(178, 292);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 82);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.label2.Location = new System.Drawing.Point(647, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 82);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label3.Location = new System.Drawing.Point(102, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(175, 46);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Игрок1: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label4.Location = new System.Drawing.Point(559, 124);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(175, 46);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Игрок2: ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(162, 398);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(609, 398);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label5.Location = new System.Drawing.Point(350, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(117, 46);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Ход: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(917, 797);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Pr5/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using System.Drawing;
+
+namespace Pr5
+{
+    public partial class Form1 : Form
+    {
+        private DiceGame diceGame = new DiceGame();
+
+        private int player1Score = 0;
+        private int player2Score = 0;
+        private int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            label3.Text += player1Score;
+            label4.Text += player2Score;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            label5.Text = "Ход: " + count.ToString();
+        }
+
+        private async Task RollDiceAndUpdateUI(int playerNumber, Label diceLabel, Label scoreLabel)
+        {
+            try
+            {
+                int diceValues = await diceGame.RollDiceAsync(6);
+
+                if (playerNumber == 1)
+                {
+                    int playerScore = diceValues + int.Parse(label3.Text.Split(' ')[1]);
+                    player1Score = playerScore;
+                    diceLabel.Text = $"?";
+                    pictureBox1.Image = Image.FromFile("Resources\\1.png");
+                    await Task.Delay(50);
+                    pictureBox1.Image = Image.FromFile("Resources\\6.png");
+                    await Task.Delay(50);
+                    pictureBox1.Image = Image.FromFile("Resources\\4.png");
+                    await Task.Delay(50);
+                    pictureBox1.Image = Image.FromFile("Resources\\3.png");
+                    await Task.Delay(50);
+                    pictureBox1.Image = Image.FromFile("Resources\\2.png");
+                    await Task.Delay(50);
+                    pictureBox1.Image = Image.FromFile("Resources\\1.png");
+                    await Task.Delay(50);
+                    pictureBox1.Image = Image.FromFile("Resources\\5.png");
+                    await Task.Delay(50);
+                    scoreLabel.Text = $"Игрок1: {player1Score}";
+                    diceLabel.Text = $"{diceValues}";
+                    switch (diceValues)
+                    {
+                        case 1:
+                            pictureBox1.Image = Image.FromFile("Resources\\1.png");
+                            break;
+                        case 2:
+                            pictureBox1.Image = Image.FromFile("Resources\\2.png");
+                            break;
+                        case 3:
+                            pictureBox1.Image = Image.FromFile("Resources\\3.png");
+                            break;
+                        case 4:
+                            pictureBox1.Image = Image.FromFile("Resources\\4.png");
+                            break;
+                        case 5:
+                            pictureBox1.Image = Image.FromFile("Resources\\5.png");
+                            break;
+                        case 6:
+                            pictureBox1.Image = Image.FromFile("Resources\\6.png");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    int playerScore = diceValues + int.Parse(label4.Text.Split(' ')[1]);
+                    player2Score = playerScore;
+                    diceLabel.Text = $"?";
+                    pictureBox2.Image = Image.FromFile("Resources\\1.png");
+                    await Task.Delay(50);
+                    pictureBox2.Image = Image.FromFile("Resources\\6.png");
+                    await Task.Delay(50);
+                    pictureBox2.Image = Image.FromFile("Resources\\4.png");
+                    await Task.Delay(50);
+                    pictureBox2.Image = Image.FromFile("Resources\\3.png");
+                    await Task.Delay(50);
+                    pictureBox2.Image = Image.FromFile("Resources\\2.png");
+                    await Task.Delay(50);
+                    pictureBox2.Image = Image.FromFile("Resources\\1.png");
+                    await Task.Delay(50);
+                    pictureBox2.Image = Image.FromFile("Resources\\5.png");
+                    await Task.Delay(50);
+                    scoreLabel.Text = $"Игрок2: {player2Score}";
+                    diceLabel.Text = $"{diceValues}";
+                    switch (diceValues)
+                    {
+                        case 1:
+                            pictureBox2.Image = Image.FromFile("Resources\\1.png");
+                            break;
+                        case 2:
+                            pictureBox2.Image = Image.FromFile("Resources\\2.png");
+                            break;
+                        case 3:
+                            pictureBox2.Image = Image.FromFile("Resources\\3.png");
+                            break;
+                        case 4:
+                            pictureBox2.Image = Image.FromFile("Resources\\4.png");
+                            break;
+                        case 5:
+                            pictureBox2.Image = Image.FromFile("Resources\\5.png");
+                            break;
+                        case 6:
+                            pictureBox2.Image = Image.FromFile("Resources\\6.png");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                if (count >= 5)
+                {
+                    string t = "Ничья!";
+                    if (player1Score > player2Score)
+                    {
+                        t = "Игрок1 выйграл!";
+                    }
+                    else
+                    {
+                        t = "Игрок2 выйграл!";
+                    }
+                    MessageBox.Show($"Конец игры: {t}", "Конец", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await RollDiceAndUpdateUI(1, label1, label3);
+            button1.Enabled = false;
+            button2.Enabled = true;
+            count++;
+            label5.Text = "Ход: " + count.ToString();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await RollDiceAndUpdateUI(2, label2, label4);
+            button2.Enabled = false;
+            button1.Enabled = true;
+            count++;
+            label5.Text = "Ход: " + count.ToString();
+        }
+    }
+
+    public class DiceGame
+    {
+        private RandomOrgApi randomOrgApi = new RandomOrgApi();
+
+        public async Task<int> RollDiceAsync(int maxValue)
+        {
+            return await randomOrgApi.RollDiceAsync(maxValue);
+        }
+    }
+
+    public class RandomOrgApi
+    {
+        private const string ApiUrl = "https://api.random.org/json-rpc/2/invoke";
+        private const string ApiKey = "9323b628-0c5e-4056-970e-a2772571423e";
+
+        public async Task<int> RollDiceAsync(int maxValue)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var requestData = new
+                {
+                    jsonrpc = "2.0",
+                    method = "generateIntegers",
+                    @params = new
+                    {
+                        apiKey = ApiKey,
+                        n = 1, // Изменен параметр на 1 для получения одного числа
+                        min = 1,
+                        max = maxValue,
+                        replacement = true
+                    },
+                    id = Guid.NewGuid().ToString()
+                };
+
+                string jsonRequest = JsonConvert.SerializeObject(requestData);
+
+                var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+
+                try
+                {
+                    var response = await client.PostAsync(ApiUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string jsonResponse = await response.Content.ReadAsStringAsync();
+                        dynamic responseObject = JsonConvert.DeserializeObject(jsonResponse);
+
+                        if (responseObject != null && responseObject.result != null && responseObject.result.random != null && responseObject.result.random.data != null)
+                        {
+                            JArray dataArray = responseObject.result.random.data;
+
+                            // Преобразуйте JArray в массив чисел.
+                            int diceValue = dataArray[0].Value<int>(); // Получаем первый элемент массива
+
+                            // Возвращаем число, представляющее результат броска костей.
+                            return diceValue;
+                        }
+                        else
+                        {
+                            throw new HttpRequestException("Failed to parse random numbers from Random.org response");
+                        }
+                    }
+                    else
+                    {
+                        throw new HttpRequestException("Failed to retrieve random numbers from Random.org");
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    throw new HttpRequestException($"HTTP request error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    throw new HttpRequestException($"Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
